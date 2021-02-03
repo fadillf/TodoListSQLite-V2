fc94bb5b9836cde81ee246010434c9d5e73a3014
Fadillah F <fadillahfadlurrahman@gmail.com>
2021-02-03 21:31:16 +0700

    Activity

---
+++ app/src/main/java/com/lov/todolistsqlite_v2/UserRowItem.java
@@ -0,0 +1,14 @@
+package com.lov.todolistsqlite_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserRowItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_row_item);
+    }
+}
